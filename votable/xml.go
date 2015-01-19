03941f5b5365f108176b9b561e35b1b1faa62ee3
Sebastien Binet <binet@cern.ch>
2015-01-19 17:59:45 +0100

    votable: more xml impl.

--- votable/xml.go
+++ votable/xml.go
@@ -5,41 +5,83 @@ import (
 )
 
 type xmlVO struct {
-	XMLName   xml.Name      `xml:"VOTABLE"`
-	Version   string        `xml:"version,attr"`
-	Resources []xmlResource `xml:"RESOURCE"`
+	XMLName     xml.Name      `xml:"VOTABLE"`
+	Version     string        `xml:"version,attr"`
+	Resources   []xmlResource `xml:"RESOURCE"`
+	Description string        `xml:"DESCRIPTION,omitempty"`
+	Infos       []xmlInfo     `xml:"INFO,omitempty"`
 }
 
 type xmlResource struct {
-	Name   string     `xml:"name,attr"`
-	Tables []xmlTable `xml:"TABLE"`
+	Name  string `xml:"name,attr,omitempty"`
+	ID    string `xml:"ID,attr,omitempty"`
+	Type  string `xml:"type,attr,omitempty"`
+	UType string `xml:"utype,attr,omitempty"`
+
+	Description string     `xml:"DESCRIPTION,omitempty"`
+	Infos       []xmlInfo  `xml:"INFO,omitempty"`
+	Tables      []xmlTable `xml:"TABLE"`
+}
+
+type xmlInfo struct {
+	XMLName xml.Name `xml:"INFO"`
+	ID      string   `xml:"ID,attr,omitempty"`
+	Name    string   `xml:"name,attr"`
+	Value   string   `xml:"value,attr"`
+	Unit    string   `xml:"unit,attr,omitempty"`
+	XType   string   `xml:"xtype,attr,omitempty"`
+	Ref     string   `xml:"ref,attr,omitempty"`
+	UCD     string   `xml:"ucd,attr,omitempty"`
+	UType   string   `xml:"utype,attr,omitempty"`
 }
 
 type xmlTable struct {
-	Name        string       `xml:"name,attr"`
-	Description string       `xml:"DESCRIPTION"`
-	Groups      []xmlGroup   `xml:"GROUP"`
-	Param       xmlParam     `xml:"PARAM"`
-	Fields      []xmlField   `xml:"FIELD"`
-	TableData   xmlTableData `xml:"DATA>TABLEDATA"`
-	BinTable    xmlBinData   `xml:"DATA>BINARY"`
-	BinTable2   xmlBinData2  `xml:"DATA>BINARY2"`
+	Name        string        `xml:"name,attr"`
+	Description string        `xml:"DESCRIPTION"`
+	Groups      []xmlGroup    `xml:"GROUP"`
+	Param       xmlParam      `xml:"PARAM"`
+	Fields      []xmlField    `xml:"FIELD"`
+	TableData   *xmlTableData `xml:"DATA>TABLEDATA,omitempty"`
+	BinTable    *xmlBinData   `xml:"DATA>BINARY,omitempty"`
+	BinTable2   *xmlBinData2  `xml:"DATA>BINARY2,omitempty"`
 }
 
 type xmlGroup struct {
-	ID        string        `xml:"ID,attr"`
-	UType     string        `xml:"utype,attr"`
+	ID    string `xml:"ID,attr,omitempty"`
+	Name  string `xml:"name,attr,omitempty"`
+	Ref   string `xml:"ref,attr,omitempty"`
+	UCD   string `xml:"ucd,attr,omitempty"`
+	UType string `xml:"utype,attr,omitempty"`
+
 	Params    []xmlParam    `xml:"PARAM"`
+	Groups    []xmlGroup    `xml:"GROUP"`
+	ParamRefs []xmlParamRef `xml:"PARAMref"`
 	FieldRefs []xmlFieldRef `xml:"FIELDref"`
+}
+
+type xmlParamRef struct {
+	Ref   string `xml:"ref,attr"`
+	UCD   string `xml:"ucd,attr,omitempty"`
+	UType string `xml:"utype,attr,omitempty"`
 }
 
 type xmlParam struct {
 	Name      string `xml:"name,attr"`
+	ID        string `xml:"ID,attr,omitempty"`
+	Unit      string `xml:"unit,attr,omitempty"`
 	DataType  string `xml:"datatype,attr"`
-	ArraySize string `xml:"arraysize,attr"`
-	UCD       string `xml:"ucd,attr"`
-	UType     string `xml:"utype,attr"`
+	Precision int    `xml:"precision,attr,omitempty"`
+	Width     int    `xml:"width,attr,omitempty"`
+	XType     string `xml:"xtype,attr,omitempty"`
+	Ref       string `xml:"ref,attr,omitempty"`
+	UCD       string `xml:"ucd,attr,omitempty"`
+	UType     string `xml:"utype,attr,omitempty"`
 	Value     string `xml:"value,attr"`
+	ArraySize string `xml:"arraysize,attr,omitempty"`
+
+	Description string      `xml:"DESCRIPTION,omitempty"`
+	Values      []xmlValues `xml:"VALUES,omitempty"`
+	Links       []xmlLink   `xml:"LINK,omitempty"`
 }
 
 type xmlFieldRef struct {
@@ -47,23 +89,59 @@ type xmlFieldRef struct {
 }
 
 type xmlField struct {
-	Name        string  `xml:"name,attr"`
-	ID          string  `xml:"ID,attr"`
-	UCD         string  `xml:"ucd,attr"`
-	Ref         string  `xml:"ref,attr"`
-	UType       string  `xml:"utype,attr"`
-	DataType    string  `xml:"datatype,attr"`
-	ArraySize   string  `xml:"arraysize,attr"`
-	Width       int     `xml:"width,attr"`
-	Precision   int     `xml:"precision,attr"`
-	Unit        string  `xml:"unit,attr"`
-	Description string  `xml:"DESCRIPTION"`
-	Link        xmlLink `xml:"LINK,omitempty"`
+	Name        string   `xml:"name,attr"`
+	ID          string   `xml:"ID,attr,omitempty"`
+	UCD         string   `xml:"ucd,attr,omitempty"`
+	Ref         string   `xml:"ref,attr,omitempty"`
+	UType       string   `xml:"utype,attr,omitempty"`
+	DataType    string   `xml:"datatype,attr"`
+	ArraySize   string   `xml:"arraysize,attr,omitempty"`
+	Precision   int      `xml:"precision,attr,omitempty"`
+	Width       int      `xml:"width,attr,omitempty"`
+	Unit        string   `xml:"unit,attr,omitempty"`
+	Description string   `xml:"DESCRIPTION,omitempty"`
+	Link        *xmlLink `xml:"LINK,omitempty"`
+}
+
+type xmlValues struct {
+	Min     *xmlMin     `xml:"MIN,omitempty"`
+	Max     *xmlMax     `xml:"MAX,omitempty"`
+	Options []xmlOption `xml:"OPTION,omitempty"`
+
+	ID   string `xml:"ID,attr,omitempty"`
+	Type string `xml:"type,attr,omitempty"` // default: "legal"
+	Null string `xml:"null,attr,omitempty"`
+	Ref  string `xml:"ref,attr,omitempty"`
+}
+
+type xmlMin struct {
+	XMLName   xml.Name `xml:"MIN"`
+	Value     string   `xml:"value,attr"`
+	Inclusive string   `xml:"inclusive,attr,omitempty"` // default: "yes"
+}
+
+type xmlMax struct {
+	XMLName   xml.Name `xml:"MAX"`
+	Value     string   `xml:"value,attr"`
+	Inclusive string   `xml:"inclusive,attr,omitempty"` // default: "yes"
+}
+
+type xmlOption struct {
+	XMLName xml.Name `xml:"OPTION"`
+
+	Name  string `xml:"name,attr,omitempty"`
+	Value string `xml:"value,attr"`
 }
 
 type xmlLink struct {
-	Type string `xml:"type,attr"`
-	HRef string `xml:"href,attr"`
+	ID          string `xml:"ID,attr,omitempty"`
+	ContentRole string `xml:"content-role,attr,omitempty"`
+	ContentType string `xml:"content-type,attr,omitempty"`
+	Title       string `xml:"title,attr,omitempty"`
+	Value       string `xml:"value,attr,omitempty"`
+	HRef        string `xml:"href,attr,omitempty"`
+	GRef        string `xml:"gref,attr,omitempty"`
+	Action      string `xml:"action,attr,omitempty"`
 }
 
 type xmlRow struct {
